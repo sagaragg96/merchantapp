3baca7f8abdf124772e76b3146c337a3e67cde64
sagaragg96 <52231185+sagaragg96@users.noreply.github.com>
2019-06-26 13:32:47 +0530

    Updates

---
+++ precache-manifest.29992ad9574eca70737b66a7ff2910b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ee8e4c0c13a6d779a4ea8bb8373f03",
+    "url": "/merchantapp/index.html"
+  },
+  {
+    "revision": "c5d677ff3ad067c51549",
+    "url": "/merchantapp/static/css/2.1fd47d8d.chunk.css"
+  },
+  {
+    "revision": "69a022adc8af99d96c2b",
+    "url": "/merchantapp/static/css/main.23de14fa.chunk.css"
+  },
+  {
+    "revision": "c5d677ff3ad067c51549",
+    "url": "/merchantapp/static/js/2.dd03a240.chunk.js"
+  },
+  {
+    "revision": "69a022adc8af99d96c2b",
+    "url": "/merchantapp/static/js/main.4dc89621.chunk.js"
+  },
+  {
+    "revision": "e0d32fdedafa958f35f1",
+    "url": "/merchantapp/static/js/runtime~main.8741dd77.js"
+  }
+]);
